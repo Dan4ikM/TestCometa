5dafa76f20e240f5077f3f6c5580010525fdb968
Daniil <moiseev@179.ru>
2019-12-02 21:06:46 +0300

    Task2 - add counter

---
+++ TestJob_Template/Assets/Task2/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    private Text itemText;
+    private int countClicks = 0;
+    
+    private void Start()
+    {
+        itemText = GetComponentInChildren<Text>();
+        UiUpdate();
+    }
+
+    public void IncreaseCounter()
+    {
+        countClicks++;
+        UiUpdate();
+    }
+
+    private void UiUpdate()
+    {
+        itemText.text = "- Item - {" +countClicks + "} clicks";
+    }
+}

3ca3e5d9354e67b098bdca32f6e6b36737a1c10b
Daniil <moiseev@179.ru>
2019-12-02 20:55:31 +0300

    Task2 - Adding Items

---
+++ TestJob_Template/Assets/Task2/AddingItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddingItem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject itemPrefab;
+    [SerializeField]
+    private Transform parent;
+
+    public void AddItem()
+    {
+        Instantiate(itemPrefab, parent);
+    }
+}

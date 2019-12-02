e4b5bf1e7f3e6d406d9149b56d3f89420e640a6c
Daniil <moiseev@179.ru>
2019-12-02 20:43:54 +0300

    EndFirstTask

---
+++ TestJob_Template/Assets/Task1/AnimatorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    private Animator animator;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SwitchWind()
+    {
+        bool isWind = !animator.GetBool("isWind");
+        animator.SetBool("isWind", isWind);
+    }
+}
